b92a61b56755a1965e0e2bbba9b0ffbdfca9c6f8
Aaron Gershman <aegershman@gmail.com>
2020-02-19 22:41:43 -0600

    deploy

---
+++ bosh-lite/main.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+set -e
+
+case "$1" in
+c | create)
+  DEPLOY_DIR="bosh-deployment"
+  STATE_DIR="state"
+
+  bosh create-env ${DEPLOY_DIR}/bosh.yml \
+    --state ${STATE_DIR}/state.json \
+    --vars-store ${STATE_DIR}/creds.yml \
+    -o ${DEPLOY_DIR}/bosh-lite-runc.yml \
+    -o ${DEPLOY_DIR}/bosh-lite.yml \
+    -o ${DEPLOY_DIR}/jumpbox-user.yml \
+    -o ${DEPLOY_DIR}/virtualbox/cpi.yml \
+    -o ${DEPLOY_DIR}/virtualbox/outbound-network.yml \
+    -v director_name="Bosh Lite Director" \
+    -v internal_cidr=192.168.50.0/24 \
+    -v internal_gw=192.168.50.1 \
+    -v internal_ip=192.168.50.6 \
+    -v outbound_network_name=NatNetwork
+  ;;
+
+*)
+  exit 1
+  ;;
+esac
