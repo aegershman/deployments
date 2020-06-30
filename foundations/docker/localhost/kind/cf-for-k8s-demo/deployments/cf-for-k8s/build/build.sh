9292161436e21a829067f42f23a735d12011ab8c
Aaron Gershman <aegershman@gmail.com>
2020-06-30 17:43:25 -0500

    relative build path is going to be a pain, that should get resolved

--- foundations/docker/localhost/kind/cf-for-k8s-demo/deployments/cf-for-k8s/build/build.sh
+++ foundations/docker/localhost/kind/cf-for-k8s-demo/deployments/cf-for-k8s/build/build.sh
@@ -14,7 +14,7 @@ ytt)
     -f _vendir/github.com/cloudfoundry/cf-for-k8s/config-optional/add-metrics-server-components.yml \
     -f _vendir/github.com/cloudfoundry/cf-for-k8s/config-optional/patch-metrics-server.yml \
     -f ../_rendered/cf/cf-values-generated.yml \
-    -f ../../cf/user/opsfiles/cf-registry-values.yml \
+    -f ../config/user/opsfiles/cf-registry-values.yml \
     -f ../../harbor/user/opsfiles/harbor-namespace.yml \
     -f ../../harbor/user/opsfiles/harbor-virtual-service.yml \
     -f ../../jaeger-operator/user/opsfiles/jaeger-operator-namespace.yml \
