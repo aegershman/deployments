1da75e3116097f217c38413494a029e3c2b208fd
Aaron Gershman <aegershman@gmail.com>
2020-06-30 17:37:19 -0500

    going to need to clean up build scripts

--- foundations/docker/localhost/kind/cf-for-k8s-demo/deployments/cf-for-k8s/build/build.sh
+++ foundations/docker/localhost/kind/cf-for-k8s-demo/deployments/cf-for-k8s/build/build.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
 case "$1" in
-*)
-  :
+generate-values)
+  # vcap.me == nip.io == xip.io, allowing for localhost dns
+  ./generate-values.sh --silence-hack-warning --cf-domain vcap.me >../_rendered/cf/cf-values-generated.yml
   ;;
 esac
