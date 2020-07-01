2668c088f75d0493838f9569407af74287744b11
Aaron Gershman <5619476+aegershman@users.noreply.github.com>
2020-07-01 17:25:14 -0500

    clusterapi redux (#132)
    * workloads
    
    * clusterapi pulls
    
    * build goofin
    
    * add help
    
    * relative path
    
    * chmod
    
    * paths
    
    * configuration yanked
    
    * whelp I think thats a fine stopping point, I'm out

---
+++ foundations/docker/localhost/kind/cluster-api-demo/deploy-ctl.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+case "$1" in
+*)
+  # https://cluster-api.sigs.k8s.io/clusterctl/commands/config-cluster.html
+  # https://cluster-api.sigs.k8s.io/clusterctl/developers.html#additional-steps-in-order-to-use-the-docker-provider
+  exit 1
+  chmod +x ./deployments/cluster-api/build/_vendir/github.com/kubernetes-sigs/cluster-api/clusterctl-darwin-amd64
+  ./deployments/cluster-api/build/_vendir/github.com/kubernetes-sigs/cluster-api/clusterctl-darwin-amd64 -h
+  ./deployments/cluster-api/build/_vendir/github.com/kubernetes-sigs/cluster-api/clusterctl-darwin-amd64 config -h
+  ./deployments/cluster-api/build/_vendir/github.com/kubernetes-sigs/cluster-api/clusterctl-darwin-amd64 --config ./deployments/cluster-api/build/clusterctl.yaml config repositories
+  ./deployments/cluster-api/build/_vendir/github.com/kubernetes-sigs/cluster-api/clusterctl-darwin-amd64 --config ./deployments/cluster-api/build/clusterctl.yaml config provider --infrastructure aws -o yaml
+  ./deployments/cluster-api/build/_vendir/github.com/kubernetes-sigs/cluster-api/clusterctl-darwin-amd64 -v5 --config ./deployments/cluster-api/build/clusterctl.yaml config provider --infrastructure aws -o yaml
+
+  ./deployments/cluster-api/build/_vendir/github.com/kubernetes-sigs/cluster-api/clusterctl-darwin-amd64 -v5 --config ./deployments/cluster-api/build/clusterctl.yaml config provider --infrastructure aws -o yaml
+  ;;
+esac
