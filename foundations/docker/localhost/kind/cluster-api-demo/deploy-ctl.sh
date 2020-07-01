8eae8b880b3ea6b0cf98e40b596c7911451a87a2
Aaron Gershman <aegershman@gmail.com>
2020-07-01 17:14:27 -0500

    chmod

--- foundations/docker/localhost/kind/cluster-api-demo/deploy-ctl.sh
+++ foundations/docker/localhost/kind/cluster-api-demo/deploy-ctl.sh
@@ -7,6 +7,7 @@ case "$1" in
   # https://cluster-api.sigs.k8s.io/clusterctl/commands/config-cluster.html
   # https://cluster-api.sigs.k8s.io/clusterctl/developers.html#additional-steps-in-order-to-use-the-docker-provider
   exit 1
+  chmod +x ./deployments/cluster-api/build/_vendir/github.com/kubernetes-sigs/cluster-api/clusterctl-darwin-amd64
   ./deployments/cluster-api/build/_vendir/github.com/kubernetes-sigs/cluster-api/clusterctl-darwin-amd64 -h
   ;;
 esac
