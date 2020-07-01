2e0075f4a9a376b01bb74597523639320cdfa540
Aaron Gershman <aegershman@gmail.com>
2020-07-01 15:34:35 -0500

    cleanup values

--- foundations/docker/localhost/kind/cf-for-k8s-demo/deployments/prometheus-operator/build/build.sh
+++ foundations/docker/localhost/kind/cf-for-k8s-demo/deployments/prometheus-operator/build/build.sh
@@ -8,7 +8,7 @@ case "$1" in
 build)
   helm template prometheus-operator --namespace=prometheus-operator \
     "${SCRIPT_DIR}/_vendir/github.com/helm/charts/prometheus-operator/stable/prometheus-operator/" \
-    --values "${SCRIPT_DIR}/values.yml" "${SCRIPT_DIR}/_vendir/stable/prometheus-operator/"
+    --values "${SCRIPT_DIR}/values.yml"
   ;;
 
 *)
