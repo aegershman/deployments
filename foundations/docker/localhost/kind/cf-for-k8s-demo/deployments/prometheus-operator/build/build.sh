dce4789e13f54e2c00a810f39afb4ab000ec6f07
Aaron Gershman <aegershman@gmail.com>
2020-07-01 15:11:18 -0500

    vendir prometheus

--- foundations/docker/localhost/kind/cf-for-k8s-demo/deployments/prometheus-operator/build/build.sh
+++ foundations/docker/localhost/kind/cf-for-k8s-demo/deployments/prometheus-operator/build/build.sh
@@ -1,6 +1,14 @@
 #!/usr/bin/env bash
 
+set -euo pipefail
+
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
+
 case "$1" in
+build)
+  helm template prometheus-operator --namespace=prometheus-operator -f "${SCRIPT_DIR}/values.yml" "${SCRIPT_DIR}/_vendir/stable/prometheus-operator/"
+  ;;
+
 *)
   :
   ;;
