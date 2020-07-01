7d7e284ffead21809180c2c6be9b2eef9e0ba071
Aaron Gershman <5619476+aegershman@users.noreply.github.com>
2020-07-01 16:03:29 -0500

    vendir prometheus operator chart (#131)
    * vendir prometheus
    
    * prometheus vendored
    
    * cleanup values

--- foundations/docker/localhost/kind/cf-for-k8s-demo/deployments/prometheus-operator/build/build.sh
+++ foundations/docker/localhost/kind/cf-for-k8s-demo/deployments/prometheus-operator/build/build.sh
@@ -1,6 +1,16 @@
 #!/usr/bin/env bash
 
+set -euo pipefail
+
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
+
 case "$1" in
+build)
+  helm template prometheus-operator --namespace=prometheus-operator \
+    "${SCRIPT_DIR}/_vendir/github.com/helm/charts/prometheus-operator/stable/prometheus-operator/" \
+    --values "${SCRIPT_DIR}/values.yml"
+  ;;
+
 *)
   :
   ;;
