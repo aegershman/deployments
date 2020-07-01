aca581c443504f31a87a5f534976b5ecce8e9d6a
Aaron Gershman <aegershman@gmail.com>
2020-07-01 15:22:01 -0500

    prometheus vendored

--- foundations/docker/localhost/kind/cf-for-k8s-demo/deployments/prometheus-operator/build/build.sh
+++ foundations/docker/localhost/kind/cf-for-k8s-demo/deployments/prometheus-operator/build/build.sh
@@ -6,7 +6,9 @@ SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 
 case "$1" in
 build)
-  helm template prometheus-operator --namespace=prometheus-operator -f "${SCRIPT_DIR}/values.yml" "${SCRIPT_DIR}/_vendir/stable/prometheus-operator/"
+  helm template prometheus-operator --namespace=prometheus-operator \
+    "${SCRIPT_DIR}/_vendir/github.com/helm/charts/prometheus-operator/stable/prometheus-operator/" \
+    --values "${SCRIPT_DIR}/values.yml" "${SCRIPT_DIR}/_vendir/stable/prometheus-operator/"
   ;;
 
 *)
