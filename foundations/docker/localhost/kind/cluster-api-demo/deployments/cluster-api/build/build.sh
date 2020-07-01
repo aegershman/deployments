b3e549bd8eac4b4380a84d69ade875b59eccea9b
Aaron Gershman <aegershman@gmail.com>
2020-07-01 17:04:29 -0500

    build goofin

--- foundations/docker/localhost/kind/cluster-api-demo/deployments/cluster-api/build/build.sh
+++ foundations/docker/localhost/kind/cluster-api-demo/deployments/cluster-api/build/build.sh
@@ -3,6 +3,14 @@
 set -euo pipefail
 
 case "$1" in
+init)
+  # purely for testing at the moment
+  exit 1
+  ./build/_vendir/github.com/kubernetes-sigs/cluster-api/components/clusterctl-darwin-amd64 init --infrastructure aws --list-images
+  clusterctl config cluster my-cluster --kubernetes-version v1.16.3 --infrastructure:aws >my-cluster.yaml
+  clusterctl config cluster my-cluster --kubernetes-version v1.16.3 --control-plane-machine-count=3 --worker-machine-count=3 >my-cluster.yaml
+  ;;
+
 *)
   exit 1
   ;;
