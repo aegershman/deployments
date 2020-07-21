eeca5f8aae45c0d82cb51724344aaa76b05acdc8
Aaron Gershman <aegershman@gmail.com>
2020-07-21 16:07:14 -0500

    create delete getkubeconfig commands

--- foundations/aws/us-east-2/sandbox/aegershman-example-0/deploy-ctl.sh
+++ foundations/aws/us-east-2/sandbox/aegershman-example-0/deploy-ctl.sh
@@ -3,8 +3,16 @@
 set -euo pipefail
 
 case "$1" in
-eksctl-deploy)
-  eksctl create cluster -f ./eks-cluster.yaml
+eksctl-create)
+  eksctl create cluster --config-file ./eks-cluster.yaml
+  ;;
+
+eksctl-delete)
+  eksctl delete cluster -f ./eks-cluster.yaml
+  ;;
+
+eksctl-write-kubeconfig)
+  eksctl utils write-kubeconfig --region=us-east-2 --cluster=aegershman-example-0 --auto-kubeconfig
   ;;
 
 *)
