ee096dd5f7911bcb2e2c77a364cb5a39f7979aa4
Aaron Gershman <5619476+aegershman@users.noreply.github.com>
2020-07-22 17:00:04 -0500

    cf-for-k8s latest + ECR attempt (and failure) (#152)
    * pull, sync, rebuild
    
    * include info for pushing cf manifests in the example eks cluster
    
    * shuffle all the folders around into different locations for the purpose of resizing and such
    
    * modifying names of registry files and such
    
    * ecr values commented out
    
    * ecr debug script
    
    * alright, well, fine, nevermind about ECR for the moment
    
    * regenerate with latest and greatest
    
    * also setup labeling rules and such
    
    * update gh label descriptions
    
    * test one more double-star
    
    * more stars, more labels, papa johns
    
    * labels for more fine-grained details
    
    * trigger
    
    * rm preceding hashtag

--- foundations/aws/us-east-2/sandbox/aegershman-example-0/deploy-ctl.sh
+++ foundations/aws/us-east-2/sandbox/aegershman-example-0/deploy-ctl.sh
@@ -25,12 +25,44 @@ cf-for-k8s-build)
   ./build.sh build
   ;;
 
+cf-for-k8s-diff-cf-generate-values)
+  diff ./deployments/cf-for-k8s/build/_vendir/github.com/cloudfoundry/cf-for-k8s/hack/generate-values.sh ./deployments/cf-for-k8s/build/generate-values.sh
+  ;;
+
 cf-for-k8s-apply)
   kapp deploy -a cf -f ./deployments/cf-for-k8s/_rendered/cf/cf-for-k8s-rendered.yml --yes
   ;;
 
 cf-for-k8s-delete)
   kapp delete -a cf --yes
+  ;;
+
+cf-for-k8s-post-install)
+  cf api --skip-ssl-validation https://api.cf.gershman.io
+  cf auth admin $(yq r ./deployments/cf-for-k8s/_rendered/cf/cf-values-generated.yml 'cf_admin_password')
+  cf target
+  cf enable-feature-flag diego_docker
+  cf create-org test-org
+  cf create-space -o test-org test-space
+  cf target -o test-org -s test-space
+  ;;
+
+cf-for-k8s-post-install-push)
+  exit 1
+  # push an app already built via docker
+  cf push -f ./deployments/cf-for-k8s/config/user/cf-manifests/hash-browns-docker-no-routes.yml --strategy=rolling
+  cf push -f ./deployments/cf-for-k8s/config/user/cf-manifests/hash-browns-docker-routes.yml --strategy=rolling
+  cf push -f ./deployments/cf-for-k8s/config/user/cf-manifests/todo-ui-docker-routes.yml --strategy=rolling
+  # push an app from source code
+  cf push test-node-app -p ./deployments/cf-for-k8s/build/_vendir/github.com/cloudfoundry/cf-for-k8s/tests/smoke/assets/test-node-app
+  curl http://test-node-app.apps.cf.gershman.io/env
+  ;;
+
+cf-for-k8s-kpack-ecr-debug)
+  exit 1
+  # aws ecr get-login-password --region us-east-2
+  kubectl -n cf-workloads-staging describe images
+  kubectl -n cf-workloads-staging describe CustomBuilder
   ;;
 
 *)
