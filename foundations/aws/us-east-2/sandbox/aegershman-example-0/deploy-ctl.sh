174c1fa123d6cdaa2b7ec2d711af528a989df0ae
Aaron Gershman <aegershman@gmail.com>
2020-07-21 19:32:42 -0500

    external-dns association with cf-for-k8s example in aws

--- foundations/aws/us-east-2/sandbox/aegershman-example-0/deploy-ctl.sh
+++ foundations/aws/us-east-2/sandbox/aegershman-example-0/deploy-ctl.sh
@@ -15,6 +15,20 @@ eksctl-delete)
   eksctl delete cluster -f ./eks-cluster.yaml
   ;;
 
+cf-for-k8s-clean-values)
+  cd ./deployments/cf-for-k8s/build
+  ./build.sh clean-generated-cf-values
+  ;;
+
+cf-for-k8s-build)
+  cd ./deployments/cf-for-k8s/build
+  ./build.sh build
+  ;;
+
+cf-for-k8s-apply)
+  kapp deploy -a cf -f ./deployments/cf-for-k8s/_rendered/cf/cf-for-k8s-rendered.yml --yes
+  ;;
+
 *)
   exit 1
   ;;
---
+++ foundations/aws/us-east-2/sandbox/aegershman-example-0/deployments/cf-for-k8s/build/build.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+case "$1" in
+clean-generated-cf-values)
+  rm -f ../_rendered/cf/cf-vars.yaml
+  ;;
+
+generate-values)
+  ./generate-values.sh --silence-hack-warning --cf-domain cf.gershman.io >../_rendered/cf/cf-values-generated.yml
+  ;;
+
+render)
+  ytt \
+    -f _vendir/github.com/cloudfoundry/cf-for-k8s/config \
+    -f _vendir/github.com/cloudfoundry/cf-for-k8s/config-optional/remove-resource-requirements.yml \
+    -f _vendir/github.com/cloudfoundry/cf-for-k8s/config-optional/add-metrics-server-components.yml \
+    -f _vendir/github.com/cloudfoundry/cf-for-k8s/config-optional/patch-metrics-server.yml \
+    -f _vendir/github.com/cloudfoundry/cf-for-k8s/config-optional/use-external-dns-for-wildcard.yml \
+    -f ../_rendered/cf/cf-values-generated.yml \
+    -f ../config/user/opsfiles/cf-registry-values.yml \
+    >../_rendered/cf/cf-for-k8s-rendered.yml
+  ;;
+
+build)
+  ./build.sh generate-values
+  ./build.sh render
+  ;;
+
+*)
+  exit 1
+  ;;
+esac
