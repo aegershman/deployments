f69f18fb2f71e02c81ac4c95f0151d4219b9a29e
Aaron Gershman <aegershman@gmail.com>
2020-07-22 14:25:52 -0500

    shuffle all the folders around into different locations for the purpose of resizing and such

---
+++ foundations/aws/us-east-2/sandbox/aegershman-example-1/deploy-ctl.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+case "$1" in
+eksctl-create)
+  eksctl create cluster --config-file=eks-cluster.yaml
+  ;;
+
+eksctl-write-kubeconfig)
+  eksctl utils write-kubeconfig --region=us-east-2 --cluster=aegershman-example-1 --auto-kubeconfig
+  ;;
+
+eksctl-delete)
+  eksctl delete cluster -f ./eks-cluster.yaml
+  ;;
+
+*)
+  exit 1
+  ;;
+esac
