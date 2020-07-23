8b0f7ad111ddaeb407ca597a72bec30f14f0ee5d
Aaron Gershman <aegershman@gmail.com>
2020-07-23 15:12:15 -0500

    resolve vendir

--- foundations/aws/us-east-2/sandbox/aegershman-example-0/deployments/external-dns/build/build.sh
+++ foundations/aws/us-east-2/sandbox/aegershman-example-0/deployments/external-dns/build/build.sh
@@ -7,7 +7,7 @@ SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 case "$1" in
 *)
   echo "generating external-dns resource definitions..."
-  helm template --namespace=external-dns "${SCRIPT_DIR}/_vendir/external-dns" \
+  helm template --namespace=external-dns "${SCRIPT_DIR}/_vendir/github.com/bitnami/charts/bitnami/external-dns" \
     --values="${SCRIPT_DIR}/../config/helm/values.yml" |
     ytt --ignore-unknown-comments -f - \
       -f "${SCRIPT_DIR}/../config/opsfiles/external-dns-ns.yml" \
