1319048d5d0ac7d1b302454cc2b39436ab500548
Aaron Gershman <aegershman@gmail.com>
2020-07-24 01:41:10 -0500

    restructure restructure

--- foundations/aws/us-east-2/sandbox/aegershman-example-0/cluster/build/harbor/build.sh
+++ foundations/aws/us-east-2/sandbox/aegershman-example-0/cluster/build/harbor/build.sh
@@ -2,8 +2,15 @@
 
 set -e
 
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
+
 case "$1" in
 *)
-  exit 1
+  echo "generating harbor resource definitions..."
+  helm template harbor "${SCRIPT_DIR}/_vendir/harbor-helm" \
+    --values="${SCRIPT_DIR}/helm-values.yml" |
+    ytt --ignore-unknown-comments -f - \
+      -f "${SCRIPT_DIR}/namespace.yml" \
+      >"${SCRIPT_DIR}/../../config/harbor/_ytt_lib/harbor/rendered.yml"
   ;;
 esac
--- foundations/aws/us-east-2/sandbox/aegershman-example-0/cluster/build/prometheus-operator/build.sh
+++ foundations/aws/us-east-2/sandbox/aegershman-example-0/cluster/build/prometheus-operator/build.sh
@@ -2,8 +2,15 @@
 
 set -e
 
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
+
 case "$1" in
 *)
-  exit 1
+  echo "generating prometheus-operator resource definitions..."
+  helm template prometheus-operator "${SCRIPT_DIR}/_vendir/prometheus-operator-helm" \
+    --values="${SCRIPT_DIR}/helm-values.yml" |
+    ytt --ignore-unknown-comments -f - \
+      -f "${SCRIPT_DIR}/prometheus-operator-namespace.yml" \
+      >"${SCRIPT_DIR}/../../config/prometheus-operator/_ytt_lib/prometheus-operator/rendered.yml"
   ;;
 esac
