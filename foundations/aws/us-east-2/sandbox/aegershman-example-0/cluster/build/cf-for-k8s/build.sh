0a839147a203e46158375fcbae81a0220370390e
Aaron Gershman <aegershman@gmail.com>
2020-07-24 02:16:41 -0500

    folder hierarchy fixes

--- foundations/aws/us-east-2/sandbox/aegershman-example-0/cluster/build/cf-for-k8s/build.sh
+++ foundations/aws/us-east-2/sandbox/aegershman-example-0/cluster/build/cf-for-k8s/build.sh
@@ -16,11 +16,11 @@ generate-values)
 *)
   echo "generating cf-for-k8s resource definitions..."
   ytt \
-    -f ${SCRIPT_DIR}/_vendir/github.com/cloudfoundry/cf-for-k8s/config \
-    -f ${SCRIPT_DIR}/_vendir/github.com/cloudfoundry/cf-for-k8s/config-optional/remove-resource-requirements.yml \
-    -f ${SCRIPT_DIR}/_vendir/github.com/cloudfoundry/cf-for-k8s/config-optional/add-metrics-server-components.yml \
-    -f ${SCRIPT_DIR}/_vendir/github.com/cloudfoundry/cf-for-k8s/config-optional/patch-metrics-server.yml \
-    -f ${SCRIPT_DIR}/_vendir/github.com/cloudfoundry/cf-for-k8s/config-optional/use-external-dns-for-wildcard.yml \
+    -f ${SCRIPT_DIR}/_vendir/cf-for-k8s/config \
+    -f ${SCRIPT_DIR}/_vendir/cf-for-k8s/config-optional/remove-resource-requirements.yml \
+    -f ${SCRIPT_DIR}/_vendir/cf-for-k8s/config-optional/add-metrics-server-components.yml \
+    -f ${SCRIPT_DIR}/_vendir/cf-for-k8s/config-optional/patch-metrics-server.yml \
+    -f ${SCRIPT_DIR}/_vendir/cf-for-k8s/config-optional/use-external-dns-for-wildcard.yml \
     -f ${SCRIPT_DIR}/cf-values-generated.yml \
     -f ${SCRIPT_DIR}/cf-registry-values-harbor.yml \
     >"${SCRIPT_DIR}/../../config/cf-for-k8s/_ytt_lib/cf-for-k8s/rendered.yml"
