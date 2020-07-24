f1e7877a6a42b116999ee522786801886a8398cb
Aaron Gershman <aegershman@gmail.com>
2020-07-24 01:27:13 -0500

    ytt

--- foundations/aws/us-east-2/sandbox/aegershman-example-0/cluster/build/external-dns/build.sh
+++ foundations/aws/us-east-2/sandbox/aegershman-example-0/cluster/build/external-dns/build.sh
@@ -6,12 +6,11 @@ SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 
 case "$1" in
 *)
-  exit 1 # TODO just testing for the moment
   echo "generating external-dns resource definitions..."
-  helm template external-dns --namespace=external-dns "${SCRIPT_DIR}/_vendir/github.com/bitnami/charts/bitnami/external-dns" \
-    --values="${SCRIPT_DIR}/config/helm/values.yml" |
+  helm template external-dns "${SCRIPT_DIR}/_vendir/bitnami/external-dns" \
+    --values="${SCRIPT_DIR}/helm-values.yml" |
     ytt --ignore-unknown-comments -f - \
-      -f "${SCRIPT_DIR}/config/opsfiles/external-dns-ns.yml" \
-      >"${SCRIPT_DIR}/../_rendered/external-dns-rendered.yml"
+      -f "${SCRIPT_DIR}/namespace.yml" \
+      >"${SCRIPT_DIR}/../../config/external-dns/_ytt_lib/rendered.yml"
   ;;
 esac
