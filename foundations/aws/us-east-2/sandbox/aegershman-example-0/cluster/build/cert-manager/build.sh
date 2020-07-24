95f59d5750ec77f4cfaafb6f91b5e26febd62b95
Aaron Gershman <aegershman@gmail.com>
2020-07-24 13:13:55 -0500

    certmanager yanked

---
+++ foundations/aws/us-east-2/sandbox/aegershman-example-0/cluster/build/cert-manager/build.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+set -e
+
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
+
+case "$1" in
+*)
+  echo "generating cert-manager resource definitions..."
+  ytt --ignore-unknown-comments \
+    -f ${SCRIPT_DIR}/_vendir/cert-manager \
+    >"${SCRIPT_DIR}/../../config/cert-manager/_ytt_lib/cert-manager/rendered.yml"
+  ;;
+esac
