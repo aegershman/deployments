5473600e1b75681acff5315c6090cf909749cde5
Aaron Gershman <aegershman@gmail.com>
2020-07-25 17:24:27 -0500

    yugabyte

---
+++ foundations/docker/localhost/kind/yugabyte-demo/deploy-ctl.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+set -e
+
+case "$1" in
+build)
+  exit 1
+  ;;
+
+a | apply)
+  exit 1
+  ;;
+
+port-forward)
+  kubectl -n yugabyte port-forward svc/yb-masters 7000:7000
+  kubectl -n yugabyte port-forward svc/yb-tservers 9042:9042 6379:6379 5433:5433
+  ;;
+
+yugabyte-logs)
+  exit 1
+  kubectl -n yugabyte logs -f -lapp=yb-master --all-containers
+  kubectl -n yugabyte logs -f -lapp=yb-tserver --all-containers
+  kubectl -n yugabyte logs -f -lchart=yugabyte --all-containers --max-log-requests=10
+  ;;
+
+*)
+  exit 1
+  ;;
+esac
