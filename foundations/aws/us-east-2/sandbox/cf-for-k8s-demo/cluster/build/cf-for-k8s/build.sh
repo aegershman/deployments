6a67d71201203f51bb9e57981585546484c6648e
Aaron Gershman <aegershman@gmail.com>
2020-07-26 20:21:18 -0500

    manager

--- foundations/aws/us-east-2/sandbox/cf-for-k8s-demo/cluster/build/cf-for-k8s/build.sh
+++ foundations/aws/us-east-2/sandbox/cf-for-k8s-demo/cluster/build/cf-for-k8s/build.sh
@@ -5,16 +5,6 @@ set -e
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 
 case "$1" in
-extract-certificates-from-cert-manager)
-  exit 1
-  # TODO not intended for direct consumption, just for convenience
-  kubectl -n letsencrypt-staging get secrets letsencrypt-cert-tls-cf-gershman-io-staging -o json | jq -r '.data."tls.crt"' | pbcopy
-  kubectl -n letsencrypt-staging get secrets letsencrypt-cert-tls-cf-gershman-io-staging -o json | jq -r '.data."tls.key"' | pbcopy
-  #
-  kubectl -n letsencrypt-prod get secrets letsencrypt-cert-tls-cf-gershman-io-prod -o json | jq -r '.data."tls.crt"' | pbcopy
-  kubectl -n letsencrypt-prod get secrets letsencrypt-cert-tls-cf-gershman-io-prod -o json | jq -r '.data."tls.key"' | pbcopy
-  ;;
-
 clean)
   rm -f "${SCRIPT_DIR}/cf-vars.yaml"
   ;;
@@ -36,7 +26,6 @@ diff)
     -f ${SCRIPT_DIR}/_vendir/cf-for-k8s/config-optional/patch-metrics-server.yml \
     -f ${SCRIPT_DIR}/_vendir/cf-for-k8s/config-optional/use-external-dns-for-wildcard.yml \
     -f ${SCRIPT_DIR}/cf-values-generated.yml \
-    -f ${SCRIPT_DIR}/cf-certificates-from-cert-manager.yml \
     -f ${SCRIPT_DIR}/cf-registry-values-harbor.yml \
     >"${SCRIPT_DIR}/../../config/cf-for-k8s/_ytt_lib/cf-for-k8s/rendered.yml"
   ;;
