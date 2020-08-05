c4261d2e2444d4caf4277b8a9c0d456f2599c6a7
Aaron Gershman <aegershman@gmail.com>
2020-08-05 13:37:04 -0500

    include vendiring of quarks-secret

--- foundations/aws/us-east-2/sandbox/cf-for-k8s-demo/cluster/build/quarks-secret/build.sh
+++ foundations/aws/us-east-2/sandbox/cf-for-k8s-demo/cluster/build/quarks-secret/build.sh
@@ -7,9 +7,9 @@ SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 case "$1" in
 *)
   echo "generating quarks-secret resource definitions..."
-  helm template quarks-secret "${SCRIPT_DIR}/_vendir" \
+  helm template quarks-secret --namespace=cf-system "${SCRIPT_DIR}/_vendir/deploy/helm/quarks-secret" \
     --values="${SCRIPT_DIR}/helm-values.yml" |
-    ytt --ignore-unknown-comments -f - \
-      >"${SCRIPT_DIR}/../../config/quarks-secret/_ytt_lib/quarks-secret/rendered.yml"
+    ytt --ignore-unknown-comments -f - |
+    kbld -f - >"${SCRIPT_DIR}/../../config/quarks-secret/_ytt_lib/quarks-secret/rendered.yml"
   ;;
 esac
