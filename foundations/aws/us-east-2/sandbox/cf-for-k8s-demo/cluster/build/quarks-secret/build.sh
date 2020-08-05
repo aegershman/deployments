01b917d24a21c2d8d0a21bca65639e2f94a3b097
Aaron Gershman <aegershman@gmail.com>
2020-08-05 15:09:25 -0500

    use quarks-secret specific namespace

--- foundations/aws/us-east-2/sandbox/cf-for-k8s-demo/cluster/build/quarks-secret/build.sh
+++ foundations/aws/us-east-2/sandbox/cf-for-k8s-demo/cluster/build/quarks-secret/build.sh
@@ -7,7 +7,7 @@ SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 case "$1" in
 *)
   echo "generating quarks-secret resource definitions..."
-  helm template quarks-secret --namespace=cf-system "${SCRIPT_DIR}/_vendir/deploy/helm/quarks-secret" \
+  helm template quarks-secret --namespace=quarks-secret "${SCRIPT_DIR}/_vendir/deploy/helm/quarks-secret" \
     --values="${SCRIPT_DIR}/helm-values.yml" |
     ytt --ignore-unknown-comments -f - |
     kbld -f - >"${SCRIPT_DIR}/../../config/quarks-secret/_ytt_lib/quarks-secret/rendered.yml"
