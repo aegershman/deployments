3fab231123a9a83635acc38612d98b45e33a9b1d
Aaron Gershman <5619476+aegershman@users.noreply.github.com>
2020-08-05 17:35:46 -0500

    secretgen-controller experimentation (#183)
    * add scaffolding for secretgen
    
    * vendor secretgen controller
    
    * secretgen-controller setup for have label removed
    
    * include example password yaml file for secretgen

---
+++ foundations/aws/us-east-2/sandbox/cf-for-k8s-demo/cluster/build/secretgen-controller/build.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+set -e
+
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
+
+case "$1" in
+*)
+  echo "generating secretgen-controller resource definitions..."
+  ytt \
+    -f ${SCRIPT_DIR}/_vendir/release.yml \
+    >"${SCRIPT_DIR}/../../config/secretgen-controller/_ytt_lib/secretgen-controller/rendered.yml"
+  ;;
+esac
