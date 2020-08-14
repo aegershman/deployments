d9d83b4ecf88c003eddeecfebf7bd52b081ebfd3
Aaron Gershman <5619476+aegershman@users.noreply.github.com>
2020-08-13 19:55:13 -0500

    version bumps all across the board (#197)
    * bump latest external dns
    
    * concourse and ingress nginx bumps
    
    * pull latest vendir
    
    * rebuild on content
    
    * rebuild and regenerate minibroker service catalog components
    
    * kubecf regeneration
    
    * proper regenerate kubecf
    
    * kubecf regenerated password
    
    * generate on yugabyte
    
    * pull in latest clusterapi

--- foundations/docker/localhost/kind/kubecf-demo/cluster/build/cf-operator/build.sh
+++ foundations/docker/localhost/kind/kubecf-demo/cluster/build/cf-operator/build.sh
@@ -2,8 +2,14 @@
 
 set -e
 
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
+
 case "$1" in
 *)
-  :
+  echo "generating cf-operator resource definitions..."
+  helm template cf-operator "${SCRIPT_DIR}/_vendir/upstream/cf-operator" \
+    --values="${SCRIPT_DIR}/helm-values.yml" |
+    ytt --ignore-unknown-comments -f - \
+      >"${SCRIPT_DIR}/../../config/cf-operator/_ytt_lib/cf-operator/rendered.yml"
   ;;
 esac
