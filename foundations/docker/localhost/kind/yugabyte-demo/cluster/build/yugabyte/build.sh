847fdd33e8fc89ca579433b4bd488548a4189eb8
Aaron Gershman <aegershman@gmail.com>
2020-08-13 19:50:57 -0500

    generate on yugabyte

--- foundations/docker/localhost/kind/yugabyte-demo/cluster/build/yugabyte/build.sh
+++ foundations/docker/localhost/kind/yugabyte-demo/cluster/build/yugabyte/build.sh
@@ -2,8 +2,14 @@
 
 set -e
 
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
+
 case "$1" in
 *)
-  exit 1
+  echo "generating yugabyte resource definitions..."
+  helm template yugabyte "${SCRIPT_DIR}/_vendir/upstream/stable/yugabyte" \
+    --values="${SCRIPT_DIR}/helm-values.yml" |
+    ytt --ignore-unknown-comments -f - \
+      >"${SCRIPT_DIR}/../../config/yugabyte/_ytt_lib/yugabyte/rendered.yml"
   ;;
 esac
